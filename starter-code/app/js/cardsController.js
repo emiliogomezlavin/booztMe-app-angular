16b5744c6a43cefe0490d061e7d7be579a994509
Emilio Gomez Lavin <emiliogomezlavin@gmail.com>
2016-07-11 16:19:39 -0700

    adds new card form and delete button hitting the Express Flashcards API

--- starter-code/app/js/cardsController.js
+++ starter-code/app/js/cardsController.js
@@ -1,14 +1,33 @@
+console.log("controller linked!");
+
 angular.module('CardsAgainstAssembly')
   .controller('cardsController', cardsController);
 
-function cardsController(){
-  var self = this;
-  self.all = [
-    {question: "What is Batman's guilty pleasure?"},
-    {question: "I'm sorry professor, I couldn't complete my homework because _________."},
-    {question: "I get by with a little help from _________."},
-    {question: "_________. It's a trap!"},
-    {question: "The class field trip was completely ruined by _________."},
-    {question: "What's my secret power?"}
-  ]
+function cardsController($scope, $http){
+  $scope.newCard = {};
+  $scope.addCard = addCard;
+  $scope.deleteCard = deleteCard;
+
+  function addCard(){
+    console.log($scope.newCard);
+    $http.post('https://peaceful-lowlands-15260.herokuapp.com/api/flashcards', $scope.newCard)
+      .then(function(response){
+        $scope.cards.push(response.data);
+        $scope.newCard= {};
+      });
+  }
+
+  function deleteCard(card){
+    $http.delete('https://peaceful-lowlands-15260.herokuapp.com/api/flashcards/' + card._id)
+      .then(function(response) {
+        var index = $scope.cards.indexOf(card);
+        $scope.cards.splice(index, 1);
+      });
+  }
+
+  $http.get('https://peaceful-lowlands-15260.herokuapp.com/api/flashcards')
+    .then(function(response){
+        $scope.cards = response.data;
+    });
+
 }
